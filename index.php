86e6059a2150a07ae21a9f974eab57cb85e293c4
Michael <master8.michael@yahoo.com>
2016-09-30 20:10:28 +0500

    create menu.php and index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Pokemon Go</title>
+</head>
+<body>
+<?php require ('menu.php'); echo menu::renderMenu($_GET['page']);?>
+<div class="content">
+    <a class="content_start-button" href="main.html">Start game</a>
+</div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,29 @@
+<?php
+
+class menu
+{
+    private static $items = array (
+        1 => array("name" => "Start", "href" => "index.php?page=1"),
+        2 => array("name" => "About", "href" => "index.php?page=2"),
+        3 => array("name" => "Top list", "href" => "index.php?page=3")
+    );
+
+    public static function renderMenu($selectedItem)
+    {
+        $header = '<div class="header"><div class="title"><h1><a href="main.html">Pokemon Go</a></h1></div>';
+        $menu = '<div class="menu"><ul>';
+
+        foreach(self::$items as $key => $value)
+        {
+            if ($key == $selectedItem)
+                $menu = $menu.'<li><a class="menu_selected-button"'."href=$value[href]>$value[name]</a></li>";
+            else
+                $menu = $menu.'<li><a class="menu_button"'."href=$value[href]>$value[name]</a></li>";
+        }
+
+        $menu = $menu.'</ul></div>';
+        $header = $header.$menu.'</div>';
+
+        return $header;
+    }
+}
